fa446da559dd5c7b20c6ca490151bbfdc31ce3c2
Miguel Limón <mili95_gontex@hotmail.com>
2019-03-22 00:00:36 -0600

    12:00

--- src/cli.cpp
+++ src/cli.cpp
@@ -187,7 +187,7 @@ void cli::process_commands(bool send)
 				system("sleep 0.5s");
 				break;
 		}
-		system("read");
+		system("read -n 1");
 	}
 }
 
@@ -200,7 +200,7 @@ void cli::process_text(bool send)
 	
 	capturar_string(buff_in);
 	frame.send_text(buff_in);
-	system("read");
+	system("read -n 1");
 }
 
 void cli::run()
@@ -233,7 +233,7 @@ void cli::run()
 				system("read");
 				break;
 		}
-		system("read");
+		system("read -n 1");
 	}
 }
 
